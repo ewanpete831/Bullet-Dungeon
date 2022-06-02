ecad909e5f89582ebbfb274320e508113b86eba2
ewanpete831 <ewanpete831@outlook.com>
2022-06-02 09:14:22 -0400

    Add project files.

---
+++ MenuScreen.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Bullet_Dungeon
+{
+    partial class MenuScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(252, 141);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(276, 92);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // MenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.Controls.Add(this.playButton);
+            this.Name = "MenuScreen";
+            this.Size = new System.Drawing.Size(800, 600);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+    }
+}
---
+++ MenuScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bullet_Dungeon
+{
+    public partial class MenuScreen : UserControl
+    {
+        public MenuScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            Form1.ChangeScreen(this, new GameScreen());
+        }
+    }
+}
